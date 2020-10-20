d157c1e8dd007a1a069c85793cabddbd1c06173b
Simon <silvestresimon@icloud.com>
2020-10-20 17:41:28 +0200

    filter test location

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
-fetch('https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?page=0')
-.then((response) => response.json())
-.then((data) => {
-    for(let job of data) {
-        let jobLogo = job.company_logo;
-        let jobDate = job.created_at;
-        let jobType = job.type;
-        let jobName= job.title;
-        let jobEntreprise= job.company;
-        let jobLocation= job.location;
-        let jobCard = document.createElement('div');
-        jobCard.className = "job-card"
-        jobCard.innerHTML = '<div class="job-img"><img src="' + jobLogo + '" alt="logo de l\'entreprise"></div><div class="job-hours"><p class="job-date">' + jobDate + '</p><span class="separateur"></span><p class="job-type">' + jobType + '</p></div><p class="job-name">' + jobName + '</p><p class="job-entreprise">' + jobEntreprise + '</p><small class="job-location">' + jobLocation + '</small>'
-        document.querySelector('section').append(jobCard);
-    };
-});
+let param = '';
+
+function getJob(param) {
+    fetch(`https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?${param}`)
+    .then((response) => response.json())
+    .then((data) => {
+        for(let job of data) {
+            addDataToDOM(job);
+        };
+    }); 
+}
+
  
+function addDataToDOM(job) {
+    const jobCard = document.createElement('div');
+    jobCard.className = "job-card"
+        jobCard.innerHTML = `
+            <div class="job-img"><img src="${job.company_logo}" alt="logo de l\'entreprise"></div>
+            <div class="job-hours">
+                <p class="job-date">${job.created_at}</p>
+                <span class="separateur"></span>
+                <p class="job-type">${job.type}</p>
+            </div>
+            <p class="job-name">${job.title}</p>
+            <p class="job-entreprise">${job.company}</p>
+            <small class="job-location">${job.location}</small>
+            `
+        document.querySelector('section').append(jobCard);
+}
+
+getJob();
+
+document.getElementById('bouton').addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('section').innerHTML = '';
+    param = "location=" + document.getElementById('inputLocalisation').value ;
+    getJob(param);
+})
