7f3ea200cfca8c03ad47f8f57d43becd705ac2b6
Simon <silvestresimon@icloud.com>
2020-10-22 14:14:00 +0200

    date ago and responsive

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let param = '';
 let inputSearch;
 let inputLocalisation;
 let fullTime = 'off';
+let dateActuelle = new Date();
 
 function getJob(param) {
     fetch(`https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?${param}&full_time=${fullTime}`)
@@ -13,15 +14,16 @@ function getJob(param) {
         };
     }); 
 }
-
  
 function addDataToDOM(job) {
+    let timeago = timeAgo(job);
+    
     const jobCard = document.createElement('div');
     jobCard.className = "job-card"
         jobCard.innerHTML = `
             <div class="job-img"><img src="${job.company_logo}" alt="logo de l\'entreprise"></div>
             <div class="job-hours">
-                <p class="job-date">${job.created_at}</p>
+                <p class="job-date">${timeago}</p>
                 <span class="separateur"></span>
                 <p class="job-type">${job.type}</p>
             </div>
@@ -32,30 +34,56 @@ function addDataToDOM(job) {
         document.querySelector('section').append(jobCard);
 }
 
+function timeAgo(job) {
+    let jobDate = new Date(job.created_at).getTime();
+    let tempsPasser = (dateActuelle - jobDate) / 1000;
+    var jobTempsPasser = 0;
+    
+    let jourspasser    = Math.round(tempsPasser / 86400);
+    let heuresPasser   = Math.round((tempsPasser - (jourspasser * 86400)) / 3600);
+    let minutesPasser  = Math.round((tempsPasser - (jourspasser * 86400 + heuresPasser * 3600)) / 60);
+    let secondePasser  = Math.round(tempsPasser - (jourspasser * 86400 + heuresPasser * 3600 + minutesPasser * 60));
 
-document.getElementById('bouton').addEventListener('click', (e) => {
-    e.preventDefault();
-    if(document.getElementById('inputTitle').value != ''){
-        inputSearch = "search=" + document.getElementById('inputTitle').value;
-        param = inputSearch;
-    } 
-    else if(document.getElementById('inputLocalisation').value != ''){
-        inputLocalisation = "location=" + document.getElementById('inputLocalisation').value;
-        param = inputLocalisation;
+    if (jourspasser > 0) {
+        jobTempsPasser = "il y a " + jourspasser + "j";
     }
-    else if (document.getElementById('inputTime').checked == true) {
-        fullTime = 'on';
+    else if (heuresPasser > 0) {
+        jobTempsPasser = "il y a " + heuresPasser + "h";
     }
-    else if(document.getElementById('inputTitle').value == true && document.getElementById('inputLocalisation').value == true) {
-        param = inputSearch + "&" + inputLocalisation;
+    else if (minutesPasser > 0) {
+        jobTempsPasser = "il y a " + minutesPasser + "m";
     }
-    getJob(param);
-    param = '';
-    fullTime = 'off';
-})
+    else {
+        jobTempsPasser = "il y a " + secondePasser + "s";
+    }
+    return jobTempsPasser;
+}
 
 getJob();
 
+
+[document.getElementById('bouton'), document.getElementById('boutonResponsive')].forEach(item => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+        if(document.getElementById('inputTitle').value != ''){
+            inputSearch = "search=" + document.getElementById('inputTitle').value;
+            param = inputSearch;
+        } 
+        else if(document.getElementById('inputLocalisation').value != ''){
+            inputLocalisation = "location=" + document.getElementById('inputLocalisation').value;
+            param = inputLocalisation;
+        }
+        else if (document.getElementById('inputTime').checked == true) {
+            fullTime = 'on';
+        }
+        else if(document.getElementById('inputTitle').value == true && document.getElementById('inputLocalisation').value == true) {
+            param = inputSearch + "&" + inputLocalisation;
+        }
+        getJob(param);
+        param = '';
+        fullTime = 'off';
+    })
+})
 
 window.matchMedia("(max-width: 675px)").addEventListener('change', () => {
     if (window.matchMedia("(max-width: 675px)").matches) {
